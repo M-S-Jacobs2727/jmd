660b306f57025cac02cd1e134d6a89feb0d7ad16
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-10-12 23:09:41 -0400

    update example

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -2,7 +2,7 @@ use jmd::{self, parallel};
 // TODO: inspect why syntax highlighting won't work
 // TODO: determine best API
 fn run(worker: &mut Worker) {
-    let comm = parallel::Domain::new();
+    let domain = parallel::Domain::new();
 
     let container = jmd::Container::new(
         0.0,
@@ -15,20 +15,20 @@ fn run(worker: &mut Worker) {
         jmd::PBC::PP,
         jmd::PBC::PP,
     );
-    comm.init(&container, worker.thread_ids());
+    domain.init(&container, worker.thread_ids());
 
     let mut atoms = jmd::Atoms::new();
-    let rect = jmd::region::Rect::from_box(&container);
+    let rect = jmd::Rect::from_box(&container);
     rect.add_random_atoms(&mut atoms, 10, 1, 1.0);
     let mut lj = jmd::LJCut::new();
     lj.add_coeff(1, 1, 1.0, 1.0, 2.5).unwrap();
-    let mut simulation = jmd::Simulation::new(comm);
-    let mut verlet = jmd::Verlet::new(0.005, atoms);
+    let mut simulation = jmd::Simulation::new(domain);
+    let mut verlet = jmd::Verlet::new(0.005, simulation);
     verlet.run(250);
 }
 
 fn main() -> Result<(), jmd::Error> {
-    let sim = Jmd::new();
+    let sim = jmd::Jmd::new();
 
     println!("Hello, world!");
     sim.run(2, run)
