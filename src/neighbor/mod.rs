080d1a43f230c16d8dc29d7818447008db5a41c8
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-10-18 21:55:50 -0400

    rename update_settings

--- src/neighbor/mod.rs
+++ src/neighbor/mod.rs
@@ -1,6 +1,6 @@
 mod grid;
 mod neighbor_list;
-mod neighbor_update_settings;
+mod update_settings;
 pub use grid::Grid;
 pub use neighbor_list::NeighborList;
-pub use neighbor_update_settings::UpdateSettings;
+pub use update_settings::UpdateSettings;
